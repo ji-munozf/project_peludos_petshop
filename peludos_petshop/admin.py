3b861de9490670f86679d12d4957b77ea9d2c437
ji-munozf <81991270+ji-munozf@users.noreply.github.com>
2022-05-17 16:32:10 -0400

    Modelos tipomascota, tipoproducto y producto

--- peludos_petshop/admin.py
+++ peludos_petshop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TipoMascota, TipoProducto, Producto
 
 # Register your models here.
+
+admin.site.register(TipoMascota)
+admin.site.register(TipoProducto)
+admin.site.register(Producto)
--- peludos_petshop/models.py
+++ peludos_petshop/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+#Modelo tipo_mascota
+class TipoMascota(models.Model):
+    idMascota = models.IntegerField(primary_key=True, verbose_name="ID de mascota")
+    nomTipoMascota = models.CharField(max_length=50, verbose_name="Nombre tipo de las mascotas")
+
+    def __str__(self):
+        return self.nomTipoMascota
+
+#Modelo tipo_producto
+class TipoProducto(models.Model):
+    idMascota = models.IntegerField(primary_key=True, verbose_name="ID tipo de producto")
+    nomTipoProducto = models.CharField(max_length=50, verbose_name="Nombre tipo del producto")
+
+    def __str__(self):
+        return self.nomTipoProducto
+
+class Producto(models.Model):
+    idProducto = models.IntegerField(primary_key=True, verbose_name="ID del producto")
+    nomProducto = models.CharField(max_length=100, verbose_name="Nombre del producto")
+    precio = models.IntegerField(verbose_name="Precio del producto", null=False, blank= False)
+    stock = models.IntegerField(verbose_name="Stock del producto", null=False, blank= False)
+    cantidad = models.IntegerField(verbose_name="Cantidad del producto", null=False, blank= False)
+    tipoMascota = models.ForeignKey(TipoMascota,on_delete= models.CASCADE)
+    tipoProducto = models.ForeignKey(TipoProducto,on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.nomProducto
