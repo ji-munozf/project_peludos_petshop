cbc0ed216654301384ab62783a2ac9189786e69f
ji-munozf <81991270+ji-munozf@users.noreply.github.com>
2022-06-01 22:36:10 -0400

    banner admin

--- peludos_petshop/urls.py
+++ peludos_petshop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, alimentos_secos_perro, alimentos_enlatados_perro, detalle_producto
+from .views import home, alimentos_secos_perro, alimentos_enlatados_perro, detalle_producto, home_admin
 
 urlpatterns = [
     path('', home, name="home"),
+    path('home_admin/', home_admin, name="home_admin"),
     path('alimentos_secos_perro/', alimentos_secos_perro, name="alimentos_secos_perro"),
     path('alimentos_enlatados_perro/', alimentos_enlatados_perro, name="alimentos_enlatados_perro"),
     path('detalle_producto/<str:id>', detalle_producto, name="detalle_producto"),
--- peludos_petshop/views.py
+++ peludos_petshop/views.py
@@ -8,9 +8,12 @@ from django.http import Http404
 # Create your views here.
 
 def home(request):
-    productos = Producto.objects.all()
-    contexto = {"producto": productos}
-    return render(request, 'peludos_petshop/menu_principal.html', contexto)
+
+    return render(request, 'peludos_petshop/menu_principal.html')
+
+def home_admin(request):
+
+    return render(request, 'peludos_petshop/Vista_admin/home_admin.html')
 
 def alimentos_secos_perro(request):
     productos = Producto.objects.all()
